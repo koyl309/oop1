256241fe415c63404e5ebc7b9875eca678b77e06
koyl309 <e.grigorenko@istu.edu.ua>
2025-09-16 18:01:22 +0300

    Add files via upload

---
+++ Miscevist2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2pr
+{
+    class Miscevist
+    {
+        private string relief;
+        private double ploscha;
+        private int naselennya;
+
+        public Miscevist()
+        {
+            Relief = "рівнина";
+            Ploscha = 0.0;
+            Naselennya = 0;
+        }
+
+        public string Relief
+        {
+            get => relief;
+            set => relief = value;
+        }
+
+        public double Ploscha
+        {
+            get => ploscha;
+            set => ploscha = value;
+        }
+
+        public int Naselennya
+        {
+            get => naselennya;
+            set => naselennya = value;
+        }
+
+        public void EditMiscevist(string relief, double ploscha, int naselennya)
+        {
+            this.relief = relief;
+            this.ploscha = ploscha;
+            this.naselennya = naselennya;
+
+            Console.WriteLine($"Рельєф: {this.relief}");
+            Console.WriteLine($"Площа: {this.ploscha} км^2");
+            Console.WriteLine($"Населення: {this.naselennya} осіб");
+        }
+
+        public void AnalyzeRelief()
+        {
+            if (relief.ToLower() == "рівнина")
+                Console.WriteLine("Рівнинна місцевість.");
+            else if (relief.ToLower() == "гірський")
+                Console.WriteLine("Гірська місцевість.");
+            else
+                Console.WriteLine("err");
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Miscevist misc = new Miscevist();
+
+            Console.WriteLine($"Рельєф: {misc.Relief}, Площа: {misc.Ploscha}, Населення: {misc.Naselennya}");
+            misc.AnalyzeRelief();
+
+            Console.WriteLine("\nВідредаговано:");
+            misc.EditMiscevist("гірський", 123.7, 2468);
+            misc.AnalyzeRelief();
+
+            Console.ReadLine();
+        }
+    }
+}
